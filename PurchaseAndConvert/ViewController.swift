daa60fdeec8dbd2c38bc34342997b781862ba1f2
Alan Sproat <asproat@Nerium.com>
2019-01-05 11:34:53 -0600

    Initial Commit

---
+++ PurchaseAndConvert/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PurchaseAndConvert
+//
+//  Created by Alan Sproat on 1/5/19.
+//  Copyright © 2019 Gary Sproat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
